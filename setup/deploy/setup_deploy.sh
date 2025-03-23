b496af4501b4bc5b4edc631eedc186e01a7b30f8
Amarjit Bharath <hi@amarjit.uk>
2025-03-23 02:03:15 +0000

    Owner and perms set for new deploy dir.

--- setup/deploy/setup_deploy.sh
+++ setup/deploy/setup_deploy.sh
@@ -5,6 +5,9 @@ echo -e "\n 🟩 Creating deployment script for $DOMAIN..."
 # Create deployment directory.
 echo -e "\n 🟩 Creating deployment directory..."
 mkdir -p /var/www/$DOMAIN/deploy
+chmod -R 700 /var/www/$DOMAIN/deploy
+chown -R www-data:www-data /var/www/$DOMAIN/deploy
+
 
 echo -e "\n 🟩 Creating deployment script..."
 cp deploy.sh /var/www/$DOMAIN/deploy/deploy.sh
