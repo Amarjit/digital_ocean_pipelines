3c6f7607ac55aba60be37e652a218d1d42b87c95
Amarjit Bharath <hi@amarjit.uk>
2025-03-23 03:39:20 +0000

    Fixed messaging and misc.

--- setup/setup_github_whitelist.sh
+++ setup/setup_github_whitelist.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Install JQ for JSON parsing.
+echo -e "\n 🟩  Installing JQ for JSON parsing..."
 apt install -y jq
 
-# Fetch the IPs and extract the 'hooks' section from GitHub API.
+# Fetch the IPs and extract webhook IP whitelist section from GitHub Meta API.
+echo -e "\n 🟩  Fetching GitHub webhook whitelist IPs..."
 HOOK_IPS=$(curl -s "$GITHUB_META_URL" | jq -r '.hooks[]')
 
 # Check if the response is valid
 if [[ -z "$HOOK_IPS" ]]; then
-    echo -e "\n 🟩 Failed to fetch GitHub webhook IPs."
+    echo -e "\n 🟥  Failed to fetch GitHub webhook IPs."
     exit 1
 fi
 
 # Prepare the IP list for the Apache config
+echo -e "\n 🟩  Preparing IP list for Apache configuration..."
 IP_BLOCK=""
 for ip in $HOOK_IPS; do
     IP_BLOCK="$IP_BLOCK\n            Require ip $ip"
@@ -31,13 +35,15 @@ EOF
 VHOST_FILE="/etc/apache2/sites-enabled/001-$DOMAIN-le-ssl.conf"
 
 # Check if the vhost file contains the <Directory "/var/www/$DOMAIN/public/"> block
-if ! grep -q '<Directory "/var/www/$DOMAIN/public/">' $VHOST_FILE; then
+echo -e "\n 🟩  Figuring how to add webhook whitelist to vhosts..."
+
+if ! grep -q '<Directory "/var/www/$DOMAIN/public">' $VHOST_FILE; then
     # If it doesn't exist, create the <Directory> block
     echo -e "\n 🟩 Creating <Directory> block for /var/www/$DOMAIN/public/..."
     cat <<EOF >> $VHOST_FILE
-<Directory "/var/www/$DOMAIN/public/">
-    # Your existing directives (e.g., AllowOverride, etc.)
-</Directory>
+    <Directory "/var/www/$DOMAIN/public">
+        #
+    </Directory>
 EOF
     echo "Created <Directory> block for /var/www/$DOMAIN/public/"
 fi
@@ -47,15 +53,15 @@ if grep -q '<Files "webhook.php">' $VHOST_FILE; then
     # If it exists, replace the existing block
     sed -i "/<Files \"webhook.php\">/,/<\/Files>/c\\
 $WEBHOOK_BLOCK" $VHOST_FILE
-    echo "Updated the IP whitelist block for webhook.php in the vhost file."
+    echo -e "\n 🟩  Updated the IP whitelist block for webhook.php in the vhost file."
 else
     # If not, add the block inside the <Directory> section
     sed -i "/<Directory \"\/var\/www\/$DOMAIN\/public\/\">/a\\
 $WEBHOOK_BLOCK" $VHOST_FILE
-    echo "Added the IP whitelist block for webhook.php to the vhost file."
+    echo -e "\n 🟩  Added the IP whitelist block for webhook.php to the vhost file."
 fi
 
-# Reload Apache to apply the changes
+# Reload Apache to apply the changes.
 echo -e "\n 🟩 Reloading Apache configuration..."
 systemctl reload apache2
 echo "Apache configuration reloaded."
