116b4c36650d0bbb8e8f0ff85449208bb6e854a2
Amarjit Bharath <hi@amarjit.uk>
2025-03-24 04:00:04 +0000

    Stricter permissions

--- setup/deploy/setup_deploy.sh
+++ setup/deploy/setup_deploy.sh
@@ -5,14 +5,13 @@ echo -e "\n 🟩 Creating deployment script for $DOMAIN..."
 # Create deployment directory.
 echo -e "\n 🟩 Creating deployment directory..."
 mkdir -p /var/www/$DOMAIN/deploy
-chmod -R 700 /var/www/$DOMAIN/deploy
+chmod -R 100 /var/www/$DOMAIN/deploy # execute-only
 chown -R www-data:www-data /var/www/$DOMAIN/deploy
 
 
 echo -e "\n 🟩 Creating deployment script..."
 cp setup/deploy/deploy.sh /var/www/$DOMAIN/deploy/deploy.sh
-chmod 700 /var/www/$DOMAIN/deploy/deploy.sh
-chmod +x /var/www/$DOMAIN/deploy/deploy.sh
+chmod 500 /var/www/$DOMAIN/deploy/deploy.sh # read and execute
 chown -R www-data:www-data /var/www/$DOMAIN/deploy/deploy.sh
 
 echo -e "\n ✅  Deployment script created for $DOMAIN."
