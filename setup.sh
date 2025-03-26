2b95fc5fb40b2c69dd9147f2524924e3ef20b8cc
Amarjit Bharath <hi@amarjit.uk>
2025-03-26 03:12:40 +0000

    Refactored to remove relying on env.

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ source setup/setup_env.sh
 # Get environment variables.
 source .env
 
-# Setup webhook IP whitelisting.
-chmod +x setup/setup_github_whitelist.sh
-./setup/setup_github_whitelist.sh $DOMAIN
-
 # Create deployment script.
 chmod +x setup/setup_deploy.sh
-./setup/setup_deploy.sh
+./setup/setup_deploy.sh $DOMAIN
 
 # Create webhook recipient PHP script.
 chmod +x setup/setup_webhook.sh
 ./setup/setup_webhook.sh
+
+# Setup webhook IP whitelisting.
+chmod +x setup/setup_github_whitelist.sh
+./setup/setup_webhook_whitelist.sh $DOMAIN
 
 # Notify user of setting up webhook, secret and how to test.
 echo -e "\n ✅ Setup complete"
--- setup/setup_deploy.sh
+++ setup/setup_deploy.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+$DOMAIN=$1
+
+DEPLOY_PATH="/var/www/$DOMAIN/deploy"
+DEPLOY_SCRIPT_PATH="$DEPLOY_PATH/deploy.sh"
+ARTIFACTS_PATH="$DEPLOY_PATH/artifacts"
+
+# Check if domain is provided.
+if [ -z "$DOMAIN" ]; then
+    echo -e "\n 🟥  Domain not supplied. Aborting"
+    exit 1
+fi
+
 # Check deploy directory exists.
-if [ ! -d "/var/www/$DOMAIN/deploy" ]; then
+if [ ! -d "$DEPLOY_PATH" ]; then
     echo -e "\n 🟥  Deploy directory does not exist. Aborting"
     exit 1
 fi
 
-# Paths
-DEPLOY_PATH="/var/www/$DOMAIN/deploy"
-ARTIFACTS_PATH="$DEPLOY_PATH/artifacts"
-
 echo -e "\n 🟩  Creating deployment script"
-cp setup/artifacts/deploy.sh $DEPLOY_PATH/deploy.sh
-chmod 500 $DEPLOY_PATH/deploy.sh # read and execute
-chown -R root:root $DEPLOY_PATH/deploy.sh
+cp setup/artifacts/deploy.sh $DEPLOY_SCRIPT_PATH
+chmod 100 $DEPLOY_SCRIPT_PATH # execute only
+chown -R root:root $DEPLOY_SCRIPT_PATH
 
 echo -e "\n ✅  Deployment script created"
