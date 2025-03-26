b6a7fb1c8013112fc66774a8f41410623d19996b
Amarjit Bharath <hi@amarjit.uk>
2025-03-26 04:30:21 +0000

    Bug with shell variables not available.

--- setup/setup_webhook.sh
+++ setup/setup_webhook.sh
@@ -6,6 +6,7 @@ DOMAIN_PATH="/var/www/$DOMAIN"
 LOG_PATH="/var/www/$DOMAIN/logs"
 ARTIFACTS_PATH="/var/www/$DOMAIN/deploy/artifacts"
 PUBLIC_PATH="/var/www/$DOMAIN/public/"
+DOMAIN_DEPLOY_ENV="/var/www/$DOMAIN/deploy/.env"
 
 # Check if domain is provided.
 if [ -z "$DOMAIN" ]; then
@@ -13,8 +14,15 @@ if [ -z "$DOMAIN" ]; then
     exit 1
 fi
 
+# Check domain exists
 if [ ! -d "$DOMAIN_PATH" ]; then
     echo -e "\n 🟥  Domain does not exist. Aborting"
+    exit 1
+fi
+
+# Check if domain environment variables exist.
+if [[ ! -f "$DOMAIN_DEPLOY_ENV" ]]; then
+    echo -e "\n 🟥  Domain environment variables not found. setup_env.sh must be run. Aborting"
     exit 1
 fi
 
@@ -23,6 +31,8 @@ if [ ! -d "$LOG_PATH" ]; then
     echo -e "\n 🟥  Logs directory does not exist. Aborting"
     exit 1
 fi
+
+source $DOMAIN_DEPLOY_ENV
 
 echo -e "\n 🟩  Setting up webhook recipient PHP script"
 
@@ -35,12 +45,12 @@ chmod 200 $LOG_PATH/webhook.log # write-only
 echo -e "\n 🟩  Creating webhook artifacts"
 cp setup/artifacts/webhook.php $ARTIFACTS_PATH/webhook.php
 chown root:root $ARTIFACTS_PATH/webhook.php
-chmod 100 $ARTIFACTS_PATH/webhook.php # executable only
+chmod 500 $ARTIFACTS_PATH/webhook.php # executable + read
 
 echo -e "\n 🟩  Copying webhook to domain for immediate website setup"
 cp setup/artifacts/webhook.php $PUBLIC_PATH/webhook.php
 chown www-data:www-data $PUBLIC_PATH/webhook.php
-chmod 100 $PUBLIC_PATH/webhook.php # executable only
+chmod 500 $PUBLIC_PATH/webhook.php # executable + read
 
 # Replace Github webhook secret key directly in file. It is only executable and cannot be read by www-data.
 echo -e "\n 🟩  Replacing GitHub webhook secret key in webhook.php"
